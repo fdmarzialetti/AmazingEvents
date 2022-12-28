24c80d3ad391fddd31293a78cec900c36790c7c2
fdmarzialetti <58396462+fdmarzialetti@users.noreply.github.com>
2022-12-28 17:33:44 -0300

    Add files via upload

--- assets/scripts/showPastEvents.js
+++ assets/scripts/showPastEvents.js
@@ -12,7 +12,7 @@ function createTemplate(events) {
                             <h5 class="card-title w-100 text-center">${e.name}</h5>
                             <p class="card-text h-100">${e.description}</p>
                             <div class=" card-bottom d-flex justify-content-between align-items-center w-100">
-                                <p class="fw-bold text-align-center">Price $${e.price}</p>
+                                <p class="m-0 fw-bold text-align-center">Price $${e.price}</p>
                                 <a href="./details.html" class="btn btn-danger">View more</a>
                             </div>
                         </div>
