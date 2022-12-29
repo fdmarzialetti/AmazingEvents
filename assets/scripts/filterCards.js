217f11491222854b88e14e6b1838987132be373d
fdmarzialetti <fdmarzialetti@gmail.com>
2022-12-29 18:29:33 -0300

    modificacion en filter

--- assets/scripts/filterCards.js
+++ assets/scripts/filterCards.js
@@ -5,7 +5,7 @@ let arrEventsFilter
 //addEventListener a cada checkbox 
 checkboxes.forEach(chk=>{
     chk.addEventListener("change", e => {
-        //Cada vez que un checkbox cambie, completa un arreglo con los nombres de los checked.
+        //Cuando un checkbox cambie de estado, completa un arreglo con los nombres de los chechbox checkeds.
         arrCheckNames = Array.from(checkboxes).filter(c=>c.checked).map(n=>n.name)
         //Filtra las cartas que compartan categoria con el arreglo de nombres.
         arrEventsFilter = events["events"].filter(event=>arrCheckNames.includes(event["category"]))
