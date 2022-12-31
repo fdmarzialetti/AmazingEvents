b1b742a748a36defeaa1e997ca7417760ca2f70f
fdmarzialetti <fdmarzialetti@gmail.com>
2022-12-31 17:59:20 -0300

    add href index.html in AmazingEvents Logo

--- assets/scripts/filterCards.js
+++ assets/scripts/filterCards.js
@@ -1,21 +1,21 @@
 let checkboxes = document.querySelectorAll(".btn-check");
 
-//addEventListener a cada checkbox 
+//addEventListener a cada checkbox. 
 checkboxes.forEach(chk=>{
     chk.addEventListener("change", e => {
-        let arrCheckNames
-        let arrEventsFilter
+        let arrCategories
+        let arrFilteredEvents
         let cardsContainer = document.getElementById("cards-container");
-        //Cuando un checkbox cambie de estado, completa un arreglo con los nombres de los chechbox checked.
-        arrCheckNames = Array.from(checkboxes).filter(c=>c.checked).map(c=>c.name)
-        //Filtra las cartas que compartan categoria con el arreglo de nombres.
-        arrEventsFilter = data["events"].filter(e=>arrCheckNames.includes(e["category"]))
+        //Cuando un checkbox cambie de estado, completa un arreglo con los nombres de los checkbox checked.
+        arrCategories = Array.from(checkboxes).filter(c=>c.checked).map(c=>c.name)
+        //Filtra las cartas que compartan categoria con el arreglo de categorias.
+        arrFilteredEvents = data["events"].filter(event=>arrCategories.includes(event["category"]))
         //Crea el template.
-        if(arrEventsFilter.length === 0){
+        if(arrFilteredEvents.length === 0){
             //Si no hay ningun checkbox checked muestra todos los eventos.
             cardsContainer.innerHTML=createTemplate(data['events']);
         }  else{
-            cardsContainer.innerHTML=createTemplate(arrEventsFilter); 
+            cardsContainer.innerHTML=createTemplate(arrFilteredEvents); 
         }
     })
 })
