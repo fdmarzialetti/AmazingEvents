5f49a77b12c51580139190136f7a65778fcc9490
fdmarzialetti <fdmarzialetti@gmail.com>
2023-01-02 12:54:25 -0300

    Improve filterCards.js, include function applyFilter

--- assets/scripts/filterCards.js
+++ assets/scripts/filterCards.js
@@ -1,23 +1,25 @@
 let checkboxes = document.querySelectorAll(".btn-check");
 
+function applyFilter(){
+    let arrCategories
+    let cardsContainer = document.getElementById("cards-container");
+    //Cuando un checkbox cambie de estado, completa un arreglo con los nombres de los checkbox checked.
+    arrCategories = Array.from(checkboxes).filter(chk=>chk.checked).map(chk=>chk.name)
+    if(arrCategories.length === 0){
+        //Si no hay ningun checkbox checked muestra todos los eventos.
+        cardsContainer.innerHTML=createTemplate(data['events']);
+    }  else{
+        //Filtra las cartas que compartan categoria con el arreglo de categorias.
+        //Crea el template.
+        let arrFilteredEvents
+        arrFilteredEvents = data["events"].filter(event=>arrCategories.includes(event["category"]))
+        cardsContainer.innerHTML=createTemplate(arrFilteredEvents); 
+    }
+}
+
 //addEventListener a cada checkbox. 
 checkboxes.forEach(chk=>{
-    chk.addEventListener("change", function(){
-        let arrCategories
-        let cardsContainer = document.getElementById("cards-container");
-        //Cuando un checkbox cambie de estado, completa un arreglo con los nombres de los checkbox checked.
-        arrCategories = Array.from(checkboxes).filter(chk=>chk.checked).map(chk=>chk.name)
-        if(arrCategories.length === 0){
-            //Si no hay ningun checkbox checked muestra todos los eventos.
-            cardsContainer.innerHTML=createTemplate(data['events']);
-        }  else{
-            //Filtra las cartas que compartan categoria con el arreglo de categorias.
-            //Crea el template.
-            let arrFilteredEvents
-            arrFilteredEvents = data["events"].filter(event=>arrCategories.includes(event["category"]))
-            cardsContainer.innerHTML=createTemplate(arrFilteredEvents); 
-        }
-    })
+    chk.addEventListener("change", applyFilter)
 })
 
 
