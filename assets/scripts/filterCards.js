701da8163fa2299808e87924ffab8023c8893986
fdmarzialetti <fdmarzialetti@gmail.com>
2023-01-02 12:29:06 -0300

    improve filterCards

--- assets/scripts/filterCards.js
+++ assets/scripts/filterCards.js
@@ -4,7 +4,6 @@ let checkboxes = document.querySelectorAll(".btn-check");
 checkboxes.forEach(chk=>{
     chk.addEventListener("change", function(){
         let arrCategories
-        let arrFilteredEvents
         let cardsContainer = document.getElementById("cards-container");
         //Cuando un checkbox cambie de estado, completa un arreglo con los nombres de los checkbox checked.
         arrCategories = Array.from(checkboxes).filter(chk=>chk.checked).map(chk=>chk.name)
@@ -14,6 +13,7 @@ checkboxes.forEach(chk=>{
         }  else{
             //Filtra las cartas que compartan categoria con el arreglo de categorias.
             //Crea el template.
+            let arrFilteredEvents
             arrFilteredEvents = data["events"].filter(event=>arrCategories.includes(event["category"]))
             cardsContainer.innerHTML=createTemplate(arrFilteredEvents); 
         }
