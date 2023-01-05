1baa5a5277f306837e2136148818540e5bb7ee18
fdmarzialetti <fdmarzialetti@gmail.com>
2023-01-05 13:48:27 -0300

    unify scripts in cards_n_filter.js

--- assets/scripts/cards_n_filters.js
+++ assets/scripts/cards_n_filters.js
@@ -1,4 +1,58 @@
-//Filtra todos los eventos por categoria
+//---------------CARDS FUNCTIONS--------------
+
+function addCardTemplate(card){
+    return `<div class="col col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="${card.image}" alt="${card.name} image">
+                    <div class="card-body d-flex flex-column align-items-center justify-content-between">
+                        <h5 class="card-title w-100 text-center">${card.name}</h5>
+                        <p class="card-text h-100">${card.description}</p>
+                        <div class=" card-bottom d-flex justify-content-between align-items-center w-100">
+                            <p class="m-0 fw-bold text-align-center">Price $${card.price}</p>
+                            <a href="./details.html?id=${card._id}" class="btn btn-danger">View more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+}
+
+function createTemplateCards(events){
+    let currentDate = data['currentDate'];
+    let pageName=document.getElementsByTagName('h1')[0].innerHTML;
+    let template="";
+    switch(pageName){
+        case "Home":{
+            for(let e of events){
+                template+=addCardTemplate(e);
+            }
+            return template;
+        }
+        case "Past Events":{
+            for(let e of events){
+                if(e.date < currentDate){
+                    template+=addCardTemplate(e);
+                }
+            }
+            return template;
+        }
+        case "Upcomming Events":{
+            for(let e of events){
+                if(e.date >= currentDate){
+                    template+=addCardTemplate(e);
+                }
+            }
+            return template;
+        }
+    }
+}
+
+function showCards(events){
+    let cardsContainer = document.getElementById("cards-container");
+    cardsContainer.innerHTML=createTemplateCards(events);
+}
+
+//-----------CHECKBOX/SEARCH FUNCTIONS---------
+
 function filterByCheckbox(){
     let arrCategories = document.querySelectorAll("input[type=checkbox]:checked")
     arrCategories = Array.from(arrCategories).map(chkCategory=>chkCategory.name)
@@ -13,7 +67,7 @@ function filterByCheckbox(){
         return filteredEvents; 
     }
 }
-//Filtra los eventos ya filtrados, ahora por search.
+
 function filterBySearch(events){
     //Toma el value del input search.
     let searchValue = document.getElementById("searchBar").value;
@@ -24,18 +78,18 @@ function filterBySearch(events){
     }
     return events
 }
-//Aplica las filtros de checkbox y search.
+
 function applyFilter(){
     //Aplica el filtro de checkbox y al resultado, el filtro search.
     //y Muestra las cartas filtradas.
     showCards(filterBySearch(filterByCheckbox()));
 }
-//Agrega un checkbox al template de checkboxes.
+
 function addCheckboxTemplate(chk){
     return `<input type="checkbox" class="btn-check" id="${chk}" name="${chk}">
     <label class="btn btn-outline-danger checkboxSize rounded-2" for="${chk}">${chk}</label>`
 }
-//Crea el template de chekboxes.
+
 function createTemplateCheckbox(categories){
     let template=""
     for(let c of categories){
@@ -43,7 +97,7 @@ function createTemplateCheckbox(categories){
     }
     return template;
 }
-//Inserta los checkboxes en el html.
+
 function showCheckbox(){
     //Crea un arreglo de categorias unicas mediante el objeto Set.
     let arrCategories=new Set(data["events"].map(e=>e.category))
@@ -52,18 +106,13 @@ function showCheckbox(){
     checkboxsContainer.innerHTML=createTemplateCheckbox(arrCategories)
 }
 
-//Renderiza los checkbox
+
+//---------RENDER AND LISTENERS-------
+showCards(data["events"])
 showCheckbox()
-//Agrega un escuchador de eventos al searchbar y al grupo de checkboxes.
 document.getElementById("searchBar").addEventListener("input", applyFilter);
 document.getElementById("checkboxGroup").addEventListener("change",applyFilter);
 
-//ESTUDIAR
-//  objeto Set();
-
-// location.search
-
-// URLSearchParams().get();
 
 
 
