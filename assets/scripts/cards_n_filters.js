cdeaa791de58db1fdaaa02b77789339d6cb938cf
fdmarzialetti <fdmarzialetti@gmail.com>
2023-01-05 17:30:08 -0300

    remove inecesary comments

--- assets/scripts/cards_n_filters.js
+++ assets/scripts/cards_n_filters.js
@@ -57,31 +57,22 @@ function filterByCheckbox(){
     let arrCategories = document.querySelectorAll("input[type=checkbox]:checked")
     arrCategories = Array.from(arrCategories).map(chkCategory=>chkCategory.name)
     if(arrCategories.length === 0){
-        //Si no hay ningun checkbox checked retorna todos los eventos.
         return data['events'];
     }  else{
-        //Sino..
-        //Filtra las cartas que compartan categoria con el arreglo de categorias.
-        let filteredEvents=[]
-        filteredEvents = data["events"].filter(event=>arrCategories.includes(event["category"]))
+        let filteredEvents = data["events"].filter(event=>arrCategories.includes(event["category"]))
         return filteredEvents; 
     }
 }
 
 function filterBySearch(events){
-    //Toma el value del input search.
     let searchValue = document.getElementById("searchBar").value;
-    //Si el value no es vacio.
     if(searchValue!==""){
-        //Filtra los eventos que compartan nombre con el value.
         events=events.filter(e=>e.name.toLowerCase().includes(searchValue.toLowerCase()))
     }
     return events
 }
 
 function applyFilter(){
-    //Aplica el filtro de checkbox y al resultado, el filtro search.
-    //y Muestra las cartas filtradas.
     showCards(filterBySearch(filterByCheckbox()));
 }
 
@@ -99,10 +90,8 @@ function createTemplateCheckbox(categories){
 }
 
 function showCheckbox(){
-    //Crea un arreglo de categorias unicas mediante el objeto Set.
     let arrCategories=new Set(data["events"].map(e=>e.category))
     let checkboxsContainer=document.getElementsByClassName("btn-group")[0];
-    //Inserto los checkboxs en el contenedor correspondiente. 
     checkboxsContainer.innerHTML=createTemplateCheckbox(arrCategories)
 }
 
