14bb308c70a468016b016239e0d2276166266a6b
fdmarzialetti <fdmarzialetti@gmail.com>
2023-01-06 23:49:32 -0300

    progress in stats.js script

--- assets/scripts/stats.js
+++ assets/scripts/stats.js
@@ -3,29 +3,28 @@ function percent(total,percent){
     return (percent*100)/total;
 }
 
-function assist(event){
+function audience(event){
     if(event.hasOwnProperty("assistance")){
         return event.assistance
     }
     return event.estimate
 }
 
-//Evento con mayor capacidad
 function largerCapacityTd(){
-    let eventLgCap=data["events"].sort((event1,event2)=>event2.capacity-event1.capacity)[0]
+    let eventLgCap=data["events"].sort((e1,e2)=>e2.capacity-e1.capacity)[0]
     return `${eventLgCap.name} ${eventLgCap.capacity}`
 }
-//Evento con menor porcentaje de asistencia
+
 function lowerPercentAttendanceTd(){
     let eventlwPerAtt=data["events"]
-    .sort((e1,e2)=>percent(e1.capacity,assist(e1))-percent(e2.capacity,assist(e2)))[0]
-    return `${eventlwPerAtt.name} %${percent(eventlwPerAtt.capacity, assist(eventlwPerAtt)).toFixed(2)}`
+    .sort((e1,e2)=>percent(e1.capacity,audience(e1))-percent(e2.capacity,audience(e2)))[0]
+    return `${eventlwPerAtt.name} %${percent(eventlwPerAtt.capacity, audience(eventlwPerAtt)).toFixed(2)}`
 }
-//Evento con mayor porcentaje de asistencia
+
 function higherPercentAttendanceTd(){
     let eventhgPerAtt=data["events"]
-    .sort((e1,e2)=>percent(e2.capacity,assist(e2))-percent(e1.capacity,assist(e1)))[0]
-    return `${eventhgPerAtt.name} %${percent(eventhgPerAtt.capacity, assist(eventhgPerAtt)).toFixed(2)}`
+    .sort((e1,e2)=>percent(e2.capacity,audience(e2))-percent(e1.capacity,audience(e1)))[0]
+    return `${eventhgPerAtt.name} %${percent(eventhgPerAtt.capacity, audience(eventhgPerAtt)).toFixed(2)}`
 }
 
 function addRowTemplate(item1,item2,item3){
@@ -35,7 +34,7 @@ function addRowTemplate(item1,item2,item3){
                 <td>${item3}</td>
             </tr>`
 }
-//LLena la tabla general
+
 function fillGeneralTable(){
     let generalStatsTable = document.getElementById("generalStats")
     generalStatsTable.innerHTML+=addRowTemplate(
@@ -45,6 +44,45 @@ function fillGeneralTable(){
     )
 }
 
+function timeEvents(id){
+    switch(id){
+        case "upcommingStats":
+            return data["events"].filter(e=>e.hasOwnProperty("estimate"))
+        case "pastStats":
+            return data["events"].filter(e=>e.hasOwnProperty("assistance"))
+    }  
+}
+
+function accumulator(events,categoryObject){
+    events.forEach(e => {
+        categoryObject.revenue+=e.price*audience(e)
+        categoryObject.capacity+=e.capacity
+        categoryObject.persons+=audience(e)
+    });
+    return categoryObject
+}
+
+function fillCategoryTable(tbodyContainer){
+    let filteredEvents = timeEvents(tbodyContainer.id)
+    let setCategory= new Set(filteredEvents.map(e=>e.category))
+    let categoryObject={"category":"","revenue":0,"capacity":0,"persons":0}
+    for(let cat of setCategory){
+        let eventsByCategory = filteredEvents.filter(e=>e.category===cat)
+        categoryObject.category=cat
+        categoryObject=accumulator(eventsByCategory,categoryObject)
+        tbodyContainer.innerHTML+=addRowTemplate(
+            categoryObject.category,
+            categoryObject.revenue,
+            `% ${percent(categoryObject.capacity, categoryObject.persons).toFixed(2)}`
+            )
+        categoryObject={"category":"","revenue":0,"capacity":0,"persons":""}
+    }
+}
+
 
 //---------------FILL TABLES----------------
+let upcommingTbodyContainer = document.getElementById("upcommingStats")
+let pastTbodyContainer = document.getElementById("pastStats")
 fillGeneralTable()
+fillCategoryTable(upcommingTbodyContainer)
+fillCategoryTable(pastTbodyContainer)
